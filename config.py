ba2884d1c288c5aa1f8f1847ff4c22f5022ab6e9
LUCAS33614700 <57056063+LUCAS33614700@users.noreply.github.com>
2026-08-02 02:23:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,37 @@
 import os
 
 
-# =========================================================
+# =========================
 # CONFIGURAÇÕES DO BOT
-# =========================================================
+# =========================
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 ADMIN_ID = int(
-    os.getenv("ADMIN_ID", "0")
+    os.getenv(
+        "ADMIN_ID",
+        "0"
+    )
 )
 
 
-# =========================================================
+# =========================
 # BANCO DE DADOS
-# =========================================================
+# =========================
 
 DATABASE_NAME = "bot.db"
 
 
-# =========================================================
+# =========================
 # DADOS DA LOJA
-# =========================================================
+# =========================
 
 NOME_DA_LOJA = "PLAYER STORE"
 
 
-# =========================================================
+# =========================
 # PUSHINPAY
-# =========================================================
+# =========================
 
 PUSHINPAY_TOKEN = os.getenv(
     "PUSHINPAY_TOKEN",
@@ -36,9 +39,9 @@ PUSHINPAY_TOKEN = os.getenv(
 )
 
 
-# =========================================================
+# =========================
 # SUPORTE
-# =========================================================
+# =========================
 
 SUPORTE_USERNAME = os.getenv(
     "SUPORTE_USERNAME",
@@ -46,9 +49,9 @@ SUPORTE_USERNAME = os.getenv(
 )
 
 
-# =========================================================
+# =========================
 # GRUPO DE CLIENTES
-# =========================================================
+# =========================
 
 GRUPO_CLIENTES = os.getenv(
     "GRUPO_CLIENTES",
@@ -56,25 +59,28 @@ GRUPO_CLIENTES = os.getenv(
 )
 
 
-# =========================================================
+# =========================
 # VERIFICAÇÃO
-# =========================================================
+# =========================
 
 def verificar_configuracao():
 
     if not BOT_TOKEN:
+
         raise ValueError(
-            "BOT_TOKEN não configurado"
+            "BOT_TOKEN não configurado."
         )
 
     if ADMIN_ID == 0:
+
         raise ValueError(
-            "ADMIN_ID não configurado"
+            "ADMIN_ID não configurado."
         )
 
     if not PUSHINPAY_TOKEN:
+
         raise ValueError(
-            "PUSHINPAY_TOKEN não configurado"
+            "PUSHINPAY_TOKEN não configurado."
         )
 
     return True
